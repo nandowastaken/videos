2ffcd4971fe9f51227532df4e2433eb72a5160df
nandowastaken <fernando.jorge@orla.tech>
2025-05-13 17:06:17 -0300

    feat: joins intersection

--- joins/joins.py
+++ joins/joins.py
@@ -2,22 +2,20 @@ from manim import *
 
 class Thumbnail(Scene):
     def construct(self):
-       title = Text("SQL JOINS").scale(0.6)
-       title.move_to([0, .85, 0])
+       title = Text("SQL JOINS").scale(2.5)
+       title.move_to([0, 2.5, 0])
         
-       tableA = Circle(radius=1.0, color=WHITE, fill_opacity=0)
-       tableA.move_to([0.5, -.25, 0])
+       tableA = Circle(radius=3, color=WHITE, fill_opacity=0)
+       tableA.move_to([2, -1, 0])
        
-       tableB = Circle(radius=1.0, color=WHITE, fill_opacity=0)
-       tableB.move_to([-0.5, -.25, 0])
+       tableB = Circle(radius=3, color=WHITE, fill_opacity=0)
+       tableB.move_to([-2, -1, 0])
        
        un = Intersection(tableA, tableB, color=BLUE, fill_opacity=1)
        
-       
-       
-       labelA = Text("A", color=WHITE).scale(0.5).next_to(tableA, LEFT)
-       labelB = Text("B", color=WHITE).scale(0.5).next_to(tableB, RIGHT)
-       labelAB = Text("A ∩ B", color=WHITE).scale(0.4).move_to(un)
+       labelA = Text("A", color=WHITE).scale(1.5).move_to(tableA)
+       labelB = Text("B", color=WHITE).scale(1.5).move_to(tableB)
+       labelAB = Text("A ∩ B", color=WHITE).scale(1).move_to(un)
        
        venn_group = VGroup(tableA, tableB, un, labelA, labelB, labelAB).scale(0.8)
        
@@ -29,30 +27,37 @@ class Introduction(Scene):
         self.play(Write(text))
         self.play(text.animate.shift(UP*3))
         
-        
         # Desenha as tabelas User e Country
-        userTable = Rectangle(color=BLUE, fill_opacity=0)
-        userTableLabel = Text("Usuário")
-        userTableLabel.move_to(userTable)
+        user = Rectangle(color=BLUE, fill_opacity=0)
+        userLabel = Text("Usuário").move_to(user)
         
-        countryTable = Rectangle(color=RED, fill_opacity=0)
-        countryTableLabel = Text("País")
+        country = Rectangle(color=RED, fill_opacity=0)
+        countryLabel = Text("País").move_to(country)
         
-        countryTable.move_to(RIGHT * 2.5)
-        countryTableLabel.move_to(RIGHT * 2.5)
+        country.move_to(RIGHT * 2.5)
+        countryLabel.move_to(RIGHT * 2.5)
         
-        self.play(DrawBorderThenFill(userTable))
-        self.play(Write(userTableLabel))
-        self.play(userTable.animate.shift(LEFT * 2.5), userTableLabel.animate.shift(LEFT * 2.5))
+        self.play(DrawBorderThenFill(user))
+        self.play(Write(userLabel))
+        self.play(user.animate.shift(LEFT * 2.5), userLabel.animate.shift(LEFT * 2.5))
         
-        self.play(DrawBorderThenFill(countryTable))
-        self.play(Write(countryTableLabel))
+        self.play(DrawBorderThenFill(country))
+        self.play(Write(countryLabel))
+        self.wait(2)
         
         # Desenha as tabelas se transformando em conjuntos
+        self.play(
+            Transform(user, Circle(radius=2, color=BLUE, fill_opacity=0).move_to(user)),
+            Transform(country, Circle(radius=2, color=RED, fill_opacity=0).move_to(country))
+        )
+        self.play(
+            user.animate.shift(RIGHT * .8), 
+            userLabel.animate.shift(RIGHT * 1),
+            country.animate.shift(LEFT * .8),
+            countryLabel.animate.shift(LEFT * 1),
+        )
         
+        intersection = Intersection(user, country, color=PURPLE, fill_opacity=0.5)
+        self.play(FadeIn(intersection))
         
-        self.wait(3)
-        
-        
-       
-        
+        self.wait(3) 
